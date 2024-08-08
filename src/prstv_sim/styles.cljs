078583846d2ed96c3c3fbc74c6b10a00a95cb8ee
Eoin Carney <eoincarney0@gmail.com>
2024-08-08 20:48:08 +0100

    added styles.cljs (forgot in prev commit)

---
+++ src/prstv_sim/styles.cljs
@@ -0,0 +1,23 @@
+(ns prstv-sim.styles)
+
+(def default-button "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700")
+
+(def default-button-disabled "text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center")
+
+(def default-label "block mb-2 text-sm font-medium text-gray-900 dark:text-white")
+
+(def drop-down-select "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500")
+
+(def default-input-field "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500")
+
+(def table-outer-div "relative overflow-x-auto shadow-md sm:rounded-lg")
+
+(def table-el "w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400")
+
+(def table-caption "p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800")
+
+(def table-caption-p "mt-1 text-sm font-normal text-gray-500 dark:text-gray-400")
+
+(def table-head "text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400")
+
+(def table-body "bg-white border-b dark:bg-gray-800 dark:border-gray-700")
