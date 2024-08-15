bfd97b7137b67e3c900bcb2522f475917d61d3fd
Eoin Carney <eoincarney0@gmail.com>
2024-08-15 19:48:30 +0100

    added colouring to sankey

--- src/prstv_sim/graphs.cljs
+++ src/prstv_sim/graphs.cljs
@@ -1,14 +1,13 @@
 (ns prstv-sim.graphs
-  (:require [reagent.core :as reagent]
-            [re-frame.core :as re-frame]
-            [prstv-sim.subs :as subs]
-            [prstv-sim.inputs :as inputs]
-            [clojure.set :as set]
-            ["chart.js" :refer (Chart)]
-            ["chartjs-chart-sankey" :as sankey]
-            ["chartjs-plugin-annotation" :as annotationPlugin]
-            [prstv-sim.styles :as styles]))
-
+  (:require
+   ["chart.js" :refer [Chart]]
+   [clojure.set :as set]
+   [clojure.string :as str]
+   [prstv-sim.inputs :as inputs]
+   [prstv-sim.styles :as styles]
+   [prstv-sim.subs :as subs]
+   [re-frame.core :as re-frame]
+   [reagent.core :as reagent]))
 
 (defn vega-spec-data->chartjs [vega-data]
   (let [sorted  (reverse (sort-by :percent vega-data))
@@ -171,9 +170,26 @@
 (defn sankey-data-to [from-candidate to-data]
   (map (fn [[name vs]] {:from from-candidate :to name :flow vs}) to-data))
 
-(defn sankey-chart-structure [data]
+(defn sankey-get-color [cfg key]
+  (let [{:keys [party-colours candidate-party]} cfg
+        og-keyword                              (->> (name key)
+                                                     reverse
+                                                     (drop 2)
+                                                     reverse
+                                                     str/join
+                                                     keyword)
+        colour-name  (-> og-keyword candidate-party party-colours)
+        colour (or (styles/colour-styles colour-name) "#FFF")]
+    colour))
+
+(defn sankey-chart-structure [cfg data]
+  (println data)
   {:type "sankey"
-   :data {:datasets [{:data data}]}})
+   :data {:datasets [{:data data
+                      :colorFrom (map (comp (partial sankey-get-color cfg) :from) data)
+                      :colorTo (map (comp (partial sankey-get-color cfg) :to) data)}]}})
+
+
 
 
 (defn sankey-chart-renderer [spec]
@@ -190,11 +206,13 @@
 
 (defn chartjs-sankey []
   (let [results         (re-frame/subscribe [::subs/results])
+        vote-config     (re-frame/subscribe [::subs/vote-config])
         sankey-selector (re-frame/subscribe [::subs/sankey-selector])]
     (when @sankey-selector
       (fn []
         (let [[candidate count] @sankey-selector
               c-data              (:c-data @results)
+              cfg                 @vote-config
               get-piles           (fn [n] (-> ((:counts c-data) n) :piles))
               cur-count           (get-piles count)
               next-count          (get-piles (inc count))
@@ -207,11 +225,9 @@
                                                              (append-count-keyword count))]
                                                 (update result cand (fnil inc 0))))
                                             {} tracked-ballots))
-                prev-count-tracked  (sankey-data-from (append-count-keyword candidate count) (gather-count-data prev-count (dec count)))
-                next-count-tracked  (sankey-data-to (append-count-keyword candidate count) (gather-count-data next-count (inc count)))
-                spec                (sankey-chart-structure (concat prev-count-tracked next-count-tracked))]
+              prev-count-tracked  (sankey-data-from (append-count-keyword candidate count) (gather-count-data prev-count (dec count)))
+              next-count-tracked  (sankey-data-to (append-count-keyword candidate count) (gather-count-data next-count (inc count)))
+              spec                (sankey-chart-structure cfg (concat prev-count-tracked next-count-tracked))]
             (if candidate
               [sankey-chart-renderer spec]
               [:div]))))))
-
-
